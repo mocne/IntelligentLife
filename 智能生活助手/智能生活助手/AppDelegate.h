1a36bb75781ffce2e5d907b8930c416a299463ff
mocne <kaifeng0605@sina.com>
2015-12-17 20:40:16 +0800

    Create  a blank project.

---
+++ 智能生活助手/智能生活助手/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  智能生活助手
+//
+//  Created by qingyun on 15/12/17.
+//  Copyright © 2015年 qingyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
